2213cd692f55f080f470998b8da2ad9746491e49
hamroqulovfayzali64-creator <hamroqulovfayzali64@gmail.com>
2026-08-07 19:39:40 +0300

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,19 @@
+TEXTS = {
+    "uz": {
+        "select_language": "🇺🇿 Tilni tanlang:",
+        "select_project": "📌 Loyihani tanlang:",
+        "admin_panel": "👨‍💼 Admin panel",
+        "no_project": "❌ Hozircha loyiha mavjud emas.",
+        "no_link": "🔗 Loyiha havolasi hali qo'shilmagan.",
+        "stats": "📊 BOT STATISTIKASI"
+    },
+
+    "ru": {
+        "select_language": "🇷🇺 Выберите язык:",
+        "select_project": "📌 Выберите проект:",
+        "admin_panel": "👨‍💼 Панель администратора",
+        "no_project": "❌ Пока нет проектов.",
+        "no_link": "🔗 Ссылка на проект ещё не добавлена.",
+        "stats": "📊 СТАТИСТИКА БОТА"
+    }
+}
