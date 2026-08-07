d29f44bba02b10a9fc125d4b8449b231ac27bd67
hamroqulovfayzali64-creator <hamroqulovfayzali64@gmail.com>
2026-08-07 21:06:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,117 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+import sqlite3
+from datetime import datetime
+
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram import types
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 
 TOKEN = "8615736731:AAFImAWTDRhBJAyOhXbY6D0wwysIa0Boz1c"
+
+ADMINS = [
+    7998053914,
+    1599812727
+]
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer("Bot ishlayapti ✅")
+db = sqlite3.connect("bot.db", check_same_thread=False)
+cursor = db.cursor()
 
 
-async def main():
-    await dp.start_polling(bot)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    language TEXT DEFAULT 'uz',
+    date TEXT
+)
+""")
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS projects(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    url TEXT
+)
+""")
+
+
+db.commit()
+
+
+state = {}
+
+
+lang_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="🇺🇿 O'zbek"),
+            KeyboardButton(text="🇷🇺 Русский")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+
+uz_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="📌 Loyihalar")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+
+ru_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="📌 Проекты")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+
+admin_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="➕ Loyiha qo'shish"),
+            KeyboardButton(text="📊 Statistika")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+
+async def save_user(user_id):
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO users VALUES(?,?,?)",
+        (
+            user_id,
+            "uz",
+            datetime.now().strftime("%Y-%m-%d")
+        )
+    )
+
+    db.commit()
+
+
+async def set_lang(user_id, lang):
+
+    cursor.execute(
+        "UPDATE users SET language=? WHERE user_id=?",
+        (lang, user_id)
+    )
+
+    db.commit()
