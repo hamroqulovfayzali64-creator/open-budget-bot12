199afb1a8c6cf558e212dccdc05a682c91858fd0
hamroqulovfayzali64-creator <hamroqulovfayzali64@gmail.com>
2026-08-07 19:54:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,38 @@ dp = Dispatcher()
 
 
 admin_state = {}
+async def save_user(user_id, language="uz"):
+    cursor.execute(
+        """
+        INSERT OR IGNORE INTO users(user_id, language, joined_date)
+        VALUES(?,?,?)
+        """,
+        (
+            user_id,
+            language,
+            datetime.now().strftime("%Y-%m-%d")
+        )
+    )
+    db.commit()
+
+
+async def get_language(user_id):
+    cursor.execute(
+        "SELECT language FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    result = cursor.fetchone()
+
+    if result:
+        return result[0]
+
+    return "uz"
+
+
+async def set_language(user_id, language):
+    cursor.execute(
+        "UPDATE users SET language=? WHERE user_id=?",
+        (language, user_id)
+    )
+    db.commit()
