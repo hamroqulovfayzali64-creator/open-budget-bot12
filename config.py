26036f94efc68da237bd6a568526f26dceeb12d6
hamroqulovfayzali64-creator <hamroqulovfayzali64@gmail.com>
2026-08-07 19:37:46 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMINS = [
+    7998053914,
+    1599812727
+]
+
+DATABASE = "bot.db"
+
+LANGUAGES = {
+    "uz": "🇺🇿 O'zbek",
+    "ru": "🇷🇺 Русский"
+}
